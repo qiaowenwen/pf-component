b752627f1e8ae71be2db158b54970a9972097739
王曼华 <irene_wmh@wangmanhuadeMacBook-Air.local>
2018-08-20 10:12:40 +0800

    定制checkbox组件

--- src/docs/demo/checkbox.js
+++ src/docs/demo/checkbox.js
@@ -4,10 +4,26 @@ import { Checkbox, List } from '../../components/index'
 const Item = List.Item
 
 export default class CheckboxDemo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isChecked: false
+    }
+  }
+  handleChange = evt => {
+    this.setState({
+      isChecked: evt.target.checked
+    })
+  }
   render() {
     return (
       <List renderHeader={() => 'Checkbox Component'}>
-        <Item extra={<Checkbox />}>On</Item>
+        <Item extra={<Checkbox defaultChecked />}>On Default</Item>
+        <Item extra={<Checkbox onChange={this.handleChange} />}>
+          here is {this.state.isChecked ? 'on' : 'off'}
+        </Item>
+        <Item extra={<Checkbox disabled />}>Disabled</Item>
+        <Item extra={<Checkbox disabled checked />}>Disabled On</Item>
       </List>
     )
   }
